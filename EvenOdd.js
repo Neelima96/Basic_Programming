6e947ec931a911d4ed61ea18444bdfafce06f0be
Neelima96 <neelimachaganti96@gmail.com>
2025-11-14 16:55:05 +0530

    Initial setup Done

---
+++ EvenOdd.js
@@ -0,0 +1,18 @@
+
+function evenOdd(num) {
+        if (num % 2 === 0)
+        {
+            return true; 
+        }
+        else{
+          return false;
+        }
+}
+if(evenOdd(8))
+{
+  console.log(`Even num`);
+}
+else
+{
+  console.log(`Odd num`);
+}
